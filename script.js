011cdeff81285d55f384032b4c0aaade31dd606a
suar0029 <suar0029@algonquinlive.com>
2023-03-19 05:59:09 -0400

    updates on html and css

---
+++ script.js
@@ -0,0 +1,10 @@
+var btnvar1 = document.getElementById('btnh1');
+
+function Toggle1(){
+    if (btnvar1.style.color =="black") {
+        btnvar1.style.color = "grey"
+    }
+    else{
+        btnvar1.style.color = "black"
+    }
+}
