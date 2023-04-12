cf077317a82b188a872b0e6eed35760fb49cd716
suar0029 <suar0029@algonquinlive.com>
2023-04-12 01:28:01 -0400

    index, css, javascript update

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-var btnvar1 = document.getElementById('btnh1');
+const form = document.querySelector('form');
+const imageContainer = document.createElement('div');
+const content = document.querySelector('#content');
 
-function Toggle1(){
-    if (btnvar1.style.color =="black") {
-        btnvar1.style.color = "grey"
-    }
-    else{
-        btnvar1.style.color = "black"
-    }
-}
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const dateInput = document.querySelector('#date-input').value;
+  const url = `https://api.nasa.gov/planetary/apod?date=${dateInput}&api_key=CsFFgeybCVlALbzrIyeSUjh5KfIpvF7zXrAxsMij`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const image = document.createElement('img');
+      image.src = data.url;
+      imageContainer.appendChild(image);
+      content.appendChild(imageContainer);
+    })
+    .catch(error => console.log(error));
+});
