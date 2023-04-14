fc2fa3f045a0286cb5d4204c6df757f9dea55894
suar0029 <suar0029@algonquinlive.com>
2023-04-14 00:50:45 -0400

    updates on html, and java.

--- script.js
+++ script.js
@@ -1,19 +1,60 @@
+
+
 const form = document.querySelector('form');
-const imageContainer = document.createElement('div');
 const content = document.querySelector('#content');
+const favoriteBtn = document.querySelector('#favorite-btn');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const dateInput = document.querySelector('#date-input').value;
   const url = `https://api.nasa.gov/planetary/apod?date=${dateInput}&api_key=CsFFgeybCVlALbzrIyeSUjh5KfIpvF7zXrAxsMij`;
 
+  // Remove the previous image container
+  const imageContainer = document.querySelector('#image-container');
+  if (imageContainer) {
+    content.removeChild(imageContainer);
+  }
+
   fetch(url)
     .then(response => response.json())
     .then(data => {
+      // Create a new image element and set its source to the API URL
       const image = document.createElement('img');
       image.src = data.url;
-      imageContainer.appendChild(image);
-      content.appendChild(imageContainer);
+
+      // Create a new image container, append the image to it, and append the container to the content element
+      const newImageContainer = document.createElement('div');
+      newImageContainer.setAttribute('id', 'image-container');
+      newImageContainer.appendChild(image);
+      content.appendChild(newImageContainer);
+
+      // Save the image URL to local storage when the favorite button is clicked
+      favoriteBtn.addEventListener('click', () => {
+        const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+        if (!favorites.includes(data.url)) {
+          favorites.push(data.url);
+          localStorage.setItem('favorites', JSON.stringify(favorites));
+          alert('Image added to favorites!');
+        } else {
+          alert('This image is already in favorites!');
+        }
+      });
     })
     .catch(error => console.log(error));
 });
+
+// Retrieve the favorites list from local storage
+const favoritesList = JSON.parse(localStorage.getItem('favorites')) || [];
+
+// Display the saved images on the webpage
+const favoritesListElement = document.querySelector('#favorites-list');
+favoritesListElement.innerHTML = '';
+favoritesList.forEach((url) => {
+  const image = document.createElement('img');
+  image.src = url;
+  const listItem = document.createElement('li');
+  listItem.appendChild(image);
+  favoritesListElement.appendChild(listItem);
+});
+
+
